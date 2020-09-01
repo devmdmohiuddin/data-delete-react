61560ccabc8002a9432505bc0dea643256bf98ac
mohiuddin <mohiuddin0240@gmail.com>
2020-09-01 22:24:47 +0600

    Updates

---
+++ precache-manifest.e7030d442d41062c77b219b883e53fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5a8c49e44ddb594fb78aa5c67fcbfe",
+    "url": "/data-delete-react/index.html"
+  },
+  {
+    "revision": "ffd4fd9887c595527229",
+    "url": "/data-delete-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d8eaddf9e2cd9ca9b779",
+    "url": "/data-delete-react/static/css/main.539bcf9e.chunk.css"
+  },
+  {
+    "revision": "ffd4fd9887c595527229",
+    "url": "/data-delete-react/static/js/2.7702d952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/data-delete-react/static/js/2.7702d952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8eaddf9e2cd9ca9b779",
+    "url": "/data-delete-react/static/js/main.7f62b1e5.chunk.js"
+  },
+  {
+    "revision": "e20cd1a4f5ae65e36960",
+    "url": "/data-delete-react/static/js/runtime-main.152d7281.js"
+  }
+]);
